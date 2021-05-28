faa09eaa470957f3c052be077612fedee9756b56
OmarSalah95 <omars.e.995@gmail.com>
2021-05-27 18:26:40 -0700

    Adds the course object model and has been tested

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-
+#include "./src/ObjectModels/Course.cpp"
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    cout << "working"
+         << "\n\n\n\n\n\n\n";
+    Course c = Course("ABC", 4, 'A', "Omar");
+    c.printReport();
     return 1;
 }
---
+++ src/ObjectModels/Course.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Course
+{
+public:
+    Course(string, int, char, string);
+    string getCourseCode();
+    void printReport();
+    string getInstructor();
+    int getCredits();
+    int getGPV();
+
+protected:
+    string courseCode,
+        instructor;
+    int gpv,
+        credits;
+    char letterGrade;
+};
+
+Course::Course(string code, int credit, char grade = 'N', string professor = "TBD")
+{
+    instructor = professor;
+    courseCode = code;
+    letterGrade = grade;
+    credits = credit;
+    switch (grade)
+    {
+    case 'A':
+        gpv = 4 * credits;
+        break;
+    case 'B':
+        gpv = 3 * credits;
+        break;
+    case 'C':
+        gpv = 2 * credits;
+        break;
+    case 'D':
+        gpv = 1 * credits;
+        break;
+    default:
+        gpv = 0;
+        break;
+    }
+};
+
+string Course::getCourseCode()
+{
+    return courseCode;
+};
+
+string Course::getInstructor()
+{
+    return instructor;
+};
+
+int Course::getCredits()
+{
+    return credits;
+};
+
+int Course::getGPV()
+{
+    return gpv;
+};
+
+void Course::printReport()
+{
+    cout << courseCode << setw(15) << instructor << setw(15) << to_string(credits) << setw(15) << letterGrade << setw(5) << to_string(gpv) << endl;
+};
